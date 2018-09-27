16989871bba3f521fc4e1a4a196bb611d72896a4
Administrator <632977592@qq.com>
2018-09-27 09:26:36 +0800

    完善个人信息中的接口

--- app/src/main/java/com/example/ouc/demo/utils/Constants.java
+++ app/src/main/java/com/example/ouc/demo/utils/Constants.java
@@ -11,6 +11,6 @@ public class Constants {
 
     /** 服务器地址 */
 //    public static String SERVER_BASE_URL = "http://kgj.ockeji.com/";//公网
-//    public static String SERVER_BASE_URL = "http://192.168.0.133:8080/oilcard/";//测试
+    public static String SERVER_BASE_URL = "http://192.168.0.133:8080/oilcard/";//测试
 
 }
